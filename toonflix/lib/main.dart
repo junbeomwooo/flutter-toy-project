d1522d3dab5983a686bbc556b321d4fb90f3e380
junbeomwooo <junbeom2.woo@gmail.com>
2025-09-18 04:24:22 +0200

    Completed

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,7 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
 
+// 에러가 발생해서 대용으로 작성한거
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..userAgent =
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
+  }
+}
+
 void main() {
+  // 에러가 발생해서 대용으로 작성한거
+  HttpOverrides.global = MyHttpOverrides();
+
   runApp(const App());
 }
 
